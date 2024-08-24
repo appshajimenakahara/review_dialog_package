481a6065ecfc370a19afb86da5c26ff1d66a7254
HajimeNakahara <hajimen19860627@gmail.com>
2024-08-24 16:07:16 +0900

    showReveiwDialog

--- lib/review_dialog.dart
+++ lib/review_dialog.dart
@@ -1,57 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
-class ReviewDialog {
-  final String appReviewUrl;
-  final String title;
-  final String message;
-  final String okButtonText;
-  final String cancelButtonText;
+Future<void> showReviewDialog({
+  required BuildContext context,
+  required String appReviewUrl,
+  String title = 'アプリを楽しんでいますか？',
+  String message = 'もしよろしければ、アプリストアでレビューをお願いします。',
+  String okButtonText = 'レビューする',
+  String cancelButtonText = '今はしない',
+}) async {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: Text(cancelButtonText),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            child: Text(okButtonText),
+            onPressed: () {
+              Navigator.of(context).pop();
+              _launchReviewPage(appReviewUrl);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
 
-  ReviewDialog({
-    required this.appReviewUrl,
-    required this.title,
-    required this.message,
-    required this.okButtonText,
-    required this.cancelButtonText,
-  });
-
-  Future<void> showReviewDialog(BuildContext context) async {
-    _displayReviewDialog(context);
-  }
-
-  void _displayReviewDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              child: Text(cancelButtonText),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text(okButtonText),
-              onPressed: () {
-                Navigator.of(context).pop();
-                _launchReviewPage(appReviewUrl);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _launchReviewPage(String appReviewUrl) async {
-    if (await canLaunchUrlString(appReviewUrl)) {
-      await launchUrlString(appReviewUrl);
-    } else {
-      print('Could not launch $appReviewUrl');
-    }
+Future<void> _launchReviewPage(String appReviewUrl) async {
+  if (await canLaunchUrlString(appReviewUrl)) {
+    await launchUrlString(appReviewUrl);
+  } else {
+    print('Could not launch $appReviewUrl');
   }
 }
